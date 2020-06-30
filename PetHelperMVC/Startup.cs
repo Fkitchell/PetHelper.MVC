53fdb51b4d4d077e560ce8923187929cb6e972a9
Fletcher Kitchell <fdkitchell@gmail.com>
2020-06-30 09:08:56 -0400

    Add project files.

---
+++ PetHelperMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetHelperMVC.Startup))]
+namespace PetHelperMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
