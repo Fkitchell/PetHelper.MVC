597139b86a32447d0cbc34c197908caa4dba4c5c
Fletcher Kitchell <fdkitchell@gmail.com>
2020-07-01 11:48:45 -0400

    add person, pet, petowner, serviceprovider, and appointment classes to data layer

---
+++ PetHelper.Data/Appointment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelper.Data
+{
+    public class Appointment
+    {
+        [Display(Name = "Date and Time")]
+        [Required]
+        public DateTimeOffset DateAndTime { get; set; }
+
+        [ForeignKey("PetOwner")]
+
+        [Required]
+        public Guid PetOwnerId
+        { get; set; }
+        [Display(Name = "Pet Owner")]
+        public virtual PetOwner PetOwner { get; set; }
+        public Pet Pet { get; set; }
+
+        [ForeignKey("ServiceProvider")]
+        [Display(Name = "Service Provider")]
+        [Required]
+        public Guid ServiceProviderId { get; set; }
+        public virtual ServiceProvider ServiceProvider { get; set; }
+
+        [Required]
+        [Display(Name = "Type of Service")]
+        public ServiceType ServiceType { get; set; }
+    }
+}
---
+++ PetHelper.Data/ServiceProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelper.Data
+{
+    public enum ServiceType { Walking, Sitting, Grooming, Training, }
+    public class ServiceProvider : Person
+    {
+        [Display(Name ="Pet Specialities")]
+        public List<string>  PetSpecialities { get; set; }
+        [Display(Name = "Services Offered")]
+        public List<ServiceType> ServiceTypes { get; set; }
+    }
+}
