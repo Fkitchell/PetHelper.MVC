7ee687a800e8714b36f339580d8e36a7cd137322
Fletcher Kitchell <fdkitchell@gmail.com>
2020-07-08 11:33:29 -0400

    add PetModels

---
+++ PetHelper.Models/PetModels/PetCreate.cs
@@ -0,0 +1,17 @@
+﻿using PetHelper.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelper.Models.PetModels
+{
+    public class PetCreate
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        public PetOwner PetOwner { get; set; }
+    }
+}
---
+++ PetHelper.Models/PetModels/PetEdit.cs
@@ -0,0 +1,16 @@
+﻿using PetHelper.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelper.Models.PetModels
+{
+    public class PetEdit
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public PetOwner PetOwner { get; set; }
+    }
+}
